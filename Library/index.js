47cf1f915b2c4f350e5bbee5469bbed27ed4bd96
midedev <ibrahimayomide330@gmail.com>
2022-09-16 19:28:39 +0100

    built display for books

--- Library/index.js
+++ Library/index.js
@@ -10,3 +10,27 @@
         build a book component
         display all books using Flexbox
 */
+
+async function getBooks() {
+  const response = await fetch("./books.json");
+  const books = await response.json();
+
+  return books;
+}
+
+const html = (book) => {
+  return `
+    <div class="my-books">
+        <div class="book-cover">${book.title}</div>
+        <div class="book-spine"></div>
+        <div class="book-footer"></div>
+    </div>
+        
+    `;
+};
+
+getBooks().then((books) => {
+  document.body.innerHTML = `<div class="container">${books
+    .map(html)
+    .join("")}</div>`;
+});

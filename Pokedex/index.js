872bcafcbf47854da4ab64ab1fe97ca65f15b382
midedev <ibrahimayomide330@gmail.com>
2022-09-16 09:26:35 +0100

    built display for Pokedex player info

--- Pokedex/index.js
+++ Pokedex/index.js
@@ -10,3 +10,33 @@
         Type(s), 
         Stats: HP/Attack/Defense/Speed
 */
+
+async function gameData() {
+  const response = await fetch("./pokemon.json");
+  const data = await response.json();
+
+  return data;
+}
+
+const type = (data) => {
+  if (data.type[1]) return `${data.type[0]} / ${data.type[1]}`;
+  return `${data.type[0]}`;
+};
+
+function getHtml(data) {
+  return `
+    <div class="player-id">${data.id}</div>
+    <div class="player-name">${data.name.english}</div>
+    <div class="player-type">${type(data)}</div>
+    <div class="player-stat">HP: ${data.base.HP}</div>
+    <div class="player-stat">Attack: ${data.base.Attack}</div>
+    <div class="player-stat">Defense: ${data.base.Defense}</div>
+    <div class="player-stat">Speed: ${data.base.Speed}</div>
+`;
+}
+
+gameData().then((data) => {
+  document.body.innerHTML = `<div class="container">
+  ${data.map(getHtml).join("")}
+  </div>`;
+});
